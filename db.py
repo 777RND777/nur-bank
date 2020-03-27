fc6688e263b1fe47420b20332d606776b9cf14d1
777rnd777@gmail.com <Iamboy97>
2020-03-27 21:50:05 +0600

    basic db was added

--- db.py
+++ db.py
@@ -1,5 +1,15 @@
 class User:
-    id = ""
-    name = ""
-    debt = 0
-    wait = 0
+    def __init__(self, id, name, debt, wait):
+        self.id = id
+        self.name = name
+        self.debt = debt
+        self.wait = wait
+
+
+users = [
+    User(475813457, "Aida Turyspekova", 65.8, 0),
+    User(830667168, "Gul’zinat Amentayeva", 265, 0),
+    User(294728712, "Murat Akhmetov", 100, 0),
+    User(287100650, "Nurmukhanbet Rakhimbayev", 300, 0),
+    User(364944874, "Tamerlan Salimgozhinov", 27, 0)
+]
--- main.py
+++ main.py
@@ -3,25 +3,33 @@ import telebot
 
 
 bot = telebot.TeleBot("990303016:AAEQfd5PnZsjgitwo0HvcLVLMQty47JI_WU")
+person = User(0, "user_name", 0, 0)
 
 
 @bot.message_handler(commands=["start"])
 def start_message(message):
-    bot.send_message(message.chat.id, "Приветствуем вас в НурБанке!", reply_markup=bank_keyboard)
+    global person
+    for user in users:
+        if user.id == message.from_user.id:
+            person = user
+            break
+    else:
+        person = User(message.from_user.id, message.from_user.first_name, 0, 0)
+    bot.send_message(message.chat.id, "Приветствуем вас в НурБанке, " + person.name + "!", reply_markup=bank_keyboard)
 
 
 @bot.message_handler(commands=["request"])
 def create_request(message):
     bot.send_message(message.chat.id, "Какую сумму вы хотите взять в долг?")
     # user.debt += new_debt(message)
-    bot.send_message(message.chat.id, "Сумма вашего долга состовляет: " + str(user.debt))
+    bot.send_message(message.chat.id, "Сумма вашего долга состовляет: " + str(person.debt))
 
 
 def new_debt(message):
     if message.text.isdigit():
         if message.text.endswith("000"):
-            message.text = str(int(int(message.text) / 1000))
-        return int(message.text)
+            return float(message.text) / 1000
+        return float(message.text)
     else:
         bot.send_message(message.chat.id, "Вы ввели неверное значение")
         return new_debt(message)
@@ -29,21 +37,21 @@ def new_debt(message):
 
 @bot.message_handler(commands=["payment"])
 def payment_notification(message):
-    bot.send_message(message.chat.id, "Какую сумму из вашего долга " + str(user.debt) + "k вы оплатили?")
-    user.wait += 0
+    bot.send_message(message.chat.id, "Какую сумму из вашего долга " + str(person.debt) + "k вы оплатили?")
+    person.wait += 0
 
 
 @bot.message_handler(commands=["debt"])
 def get_current_debt(message):
-    bot.send_message(message.chat.id, "Ваш долг состовляет: " + str(user.debt) + "k")
-    bot.send_message(message.chat.id, "Из этой суммы ожидает подтверждения: " + str(user.wait) + "k")
+    bot.send_message(message.chat.id, "Ваш долг состовляет: " + str(person.debt) + "k")
+    bot.send_message(message.chat.id, "Из этой суммы ожидает подтверждения: " + str(person.wait) + "k")
 
 
 @bot.message_handler(commands=["set_name"])
 def set_name(message):
     bot.send_message(message.chat.id, "Как вас зовут?")
-    user.name = "murat"
-    # bot.send_message(message.chat.id, "Здравствуйте, " + murat.name + "! Что вы хотите сделать?")
+    person.name = ""
+    # bot.send_message(message.chat.id, "Ваше имя изменено на '" + murat.name + "'. Что вы хотите сделать?")
 
 
 @bot.message_handler(content_types=["text"])
@@ -58,7 +66,6 @@ def send_text(message):
         set_name(message)
 
 
-user = User()
 bank_keyboard = telebot.types.ReplyKeyboardMarkup()
 bank_keyboard.row_width = 2
 bank_keyboard.row("оставить заявку на долг", "уведомить об оплате долга")

0c3d69c35d9c8739f931f81112a10f2c15ae8f6f
Nurmukhanbet Rakhimbayev <rakhimbayev.n@tntsecure.kz>
2020-04-24 14:18:06 +0600

    reading db was added

--- db.py
+++ db.py
@@ -1,16 +1,30 @@
+from openpyxl import load_workbook
+
+
 class User:
-    def __init__(self, id, name, debt, wait):
-        self.id = id
-        self.name = name
-        self.username = name
+    def __init__(self, id_number, first_name, last_name, username, debt, check, approve):
+        self.id = id_number
+        self.first_name = first_name
+        self.last_name = last_name
+        self.username = username
         self.debt = debt
-        self.wait = wait
+        self.check = check
+        self.approve = approve
 
 
-users = [
-    User(475813457, "Aida Turyspekova", 65.8, 0.0),
-    User(830667168, "Gul’zinat Amentayeva", 265.0, 0.0),
-    User(294728712, "Murat Akhmetov", 100.0, 0.0),
-    User(287100650, "Nurmukhanbet Rakhimbayev", 300.0, 0.0),
-    User(364944874, "Tamerlan Salimgozhinov", 27.0, 0.0)
-]
+wb = load_workbook('./DB.xlsx')
+sheet = wb.get_sheet_by_name('Sheet1')
+users = []
+i = 2
+while sheet["B" + str(i)].value:
+    user = User(
+        sheet["B" + str(i)].value,
+        sheet["C" + str(i)].value,
+        sheet["D" + str(i)].value,
+        sheet["E" + str(i)].value,
+        sheet["F" + str(i)].value,
+        sheet["G" + str(i)].value,
+        sheet["H" + str(i)].value,
+    )
+    users.append(user)
+    i += 1

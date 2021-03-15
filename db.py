c1a821f647d5836499d850fa0a949b7857c5dce5
Nurmukhanbet Rakhimbayev <rakhimbayev.n@tntsecure.kz>
2021-03-15 22:44:48 +0600

    slashes were added

--- db.py
+++ db.py
@@ -4,30 +4,30 @@ from bank import client
 
 
 def get_all_users():
-    c = client.get("users")
+    c = client.get("/users")
     return c.get_json()
 
 
 def get_user(user_id: int):
-    c = client.get(f"users/{user_id}")
+    c = client.get(f"/users/{user_id}")
     return c.get_json()
 
 
 def create_user(user: dict):
-    c = client.post("users", json=user)
+    c = client.post("/users", json=user)
     return c.get_json()
 
 
 def change_user(user_id: int, json: dict):
-    _ = client.put(f"users/{user_id}", json=json)
+    _ = client.put(f"/users/{user_id}", json=json)
 
 
 def create_application(json: dict):
-    _ = client.post("applications", json=json)
+    _ = client.post("/applications", json=json)
 
 
 def get_user_pending_loans(user_id):
-    c = client.get(f"users/{user_id}")
+    c = client.get(f"/users/{user_id}")
     value = 0
     for application in c.get_json()["applications"]:
         if application["value"] > 0 and not application["approved"]:
@@ -36,7 +36,7 @@ def get_user_pending_loans(user_id):
 
 
 def get_user_pending_payments(user_id):
-    c = client.get(f"users/{user_id}")
+    c = client.get(f"/users/{user_id}")
     value = 0
     for application in c.get_json()["applications"]:
         if application["value"] < 0 and not application["approved"]:
@@ -45,7 +45,7 @@ def get_user_pending_payments(user_id):
 
 
 def get_user_pending_loan_amount(user_id):
-    c = client.get(f"users/{user_id}")
+    c = client.get(f"/users/{user_id}")
     amount = 0
     for application in c.get_json()["applications"]:
         if application["value"] > 0 and not application["approved"]:
